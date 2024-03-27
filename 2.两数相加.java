d7c15f811b37bd8cd57bab1f2bd189f6f0c9b840
eclipse <1505703611@qq.com>
2024-03-27 13:18:03 +0000

    Update 2.两数相加.java

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -17,18 +17,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // create two pointers to store the head and tail of the result
         ListNode head = null, tail = null;
+        // create a carry to store the carry value
         int carry = 0;
+
+        // loop the two linked list
         while (l1 != null || l2 != null) {
+            // get the value of the two nodes
             int n1 = l1 != null ? l1.val : 0;
             int n2 = l2 != null ? l2.val : 0;
+            // calculate the sum of current two nodes and carry
             int sum = n1 + n2 + carry;
+
+            // create a new node to store the sum value
             if (head == null) {
                 head = tail = new ListNode(sum % 10);
             } else {
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
+
+            // update the carry value
             carry = sum / 10;
             if (l1 != null) {
                 l1 = l1.next;
